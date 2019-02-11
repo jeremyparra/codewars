dc731bc2dc27b926a5e969030bfd66343493ae26
Jeremy Parra <jeremydeanparra@gmail.com>
2019-02-11 14:01:09 -0700

    fixed .md file

--- Categorize New Member/app.js
+++ Categorize New Member/app.js
@@ -1,14 +1,13 @@
 function openOrSenior(input) {
     var results = [];
     input.forEach(player => {
-          var age = player[0];
-          var handycap = player[1];
-          if (age >= 55 && handycap > 7) {
-              results.push("Senior");
-          } else {
-              results.push("Open");
-          }
-      });
-      console.log("doodod");
-    //   return results;
-  }
+        var age = player[0];
+        var handycap = player[1];
+        if (age >= 55 && handycap > 7) {
+            results.push("Senior");
+        } else {
+            results.push("Open");
+        }
+    });
+    return results;
+}
